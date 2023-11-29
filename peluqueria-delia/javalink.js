9a6ce0fea1d147bf217c8b20ecd8a21197f64c4f
ORTIZFLOR123 <151787818+ORTIZFLOR123@users.noreply.github.com>
2023-11-29 12:36:31 -0300

    Add files via upload

---
+++ peluqueria-delia/javalink.js
@@ -0,0 +1,7 @@
+const fabars=document.querySelector ('.fa-bars');
+const navbar=document.querySelector('.navbar');
+
+
+fabars.addEventListener("click",()=>{
+    navbar.classList.toggle ('clase-menu')
+});
